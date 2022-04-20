0c1a5b87839a6e65248ba0493a936ec0d4c75a42
Jessica.W <jessica.watson1@live.com>
2022-04-20 14:57:44 -0600

    competed lesson 1 added dynamic variables

---
+++ learn-svelte/src/routes/index.svelte
@@ -0,0 +1,28 @@
+<script>
+	let title = 'Jessicas Super Cool Website';
+
+	const updateTitle = () => {
+		title = 'Maybe its not so cool after all';
+	};
+</script>
+
+<div class="index">
+	<h2>{title}</h2>
+	<p>
+		Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit incidunt repellendus totam
+		vel illo voluptatum maxime. Inventore necessitatibus ex magnam harum ab quae aut quod. Pariatur
+		asperiores quos nihil id, deleniti accusantium atque necessitatibus mollitia suscipit adipisci
+		illum sit repudiandae quia magnam quas error libero, eum explicabo! Asperiores, dolorem
+		provident.
+	</p>
+	<button on:click={updateTitle} />
+	<input type="text" bind:value={title} />
+</div>
+
+<style>
+	.index {
+		text-align: center;
+		display: block;
+		margin: 20px auto;
+	}
+</style>
