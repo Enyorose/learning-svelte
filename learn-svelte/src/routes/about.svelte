a78a7fc41169d4466245631a09159b639ec5634a
Jessica.W <jessica.watson1@live.com>
2022-04-20 15:17:26 -0600

    completed lesson 2 learning about routes

---
+++ learn-svelte/src/routes/about.svelte
@@ -0,0 +1,19 @@
+<div class="about">
+	<h2>About us</h2>
+	<p>
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti animi tempore quis maiores
+		ratione nihil illum hic minus ipsa distinctio ipsam impedit, eaque harum officiis reprehenderit
+		quidem omnis corrupti magni sequi. Repellat in, labore maxime, voluptatum perferendis explicabo
+		fuga corporis officiis expedita aperiam laboriosam. Officiis laborum maxime nemo earum aperiam.
+	</p>
+	<a href="/guides">View Guides</a>
+	<a href="/">Home Page</a>
+</div>
+
+<style>
+	.about {
+		text-align: center;
+		display: block;
+		margin: 20px auto;
+	}
+</style>
--- learn-svelte/src/routes/index.svelte
+++ learn-svelte/src/routes/index.svelte
@@ -1,13 +1,8 @@
 <script>
-	let title = 'Jessicas Super Cool Website';
-
-	const updateTitle = () => {
-		title = 'Maybe its not so cool after all';
-	};
 </script>
 
 <div class="index">
-	<h2>{title}</h2>
+	<h2>Welcome!</h2>
 	<p>
 		Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit incidunt repellendus totam
 		vel illo voluptatum maxime. Inventore necessitatibus ex magnam harum ab quae aut quod. Pariatur
@@ -15,8 +10,8 @@
 		illum sit repudiandae quia magnam quas error libero, eum explicabo! Asperiores, dolorem
 		provident.
 	</p>
-	<button on:click={updateTitle} />
-	<input type="text" bind:value={title} />
+	<a href="/guides">View Guides</a>
+	<a href="/about">About Us</a>
 </div>
 
 <style>
